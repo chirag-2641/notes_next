09afe6652cc29a68e01786ca08e46139e03e1b8b
chirag-2641 <chirag2641@gmail.com>
2024-05-27 00:44:21 +0530

    reverted_back

--- src/components/Login.js
+++ src/components/Login.js
@@ -19,7 +19,7 @@ const Login = (props) => {
   
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch(`http://notes-next-backend.vercel.app/api/auth/login`, {
+    const response = await fetch(`https://notes-next-backend.vercel.app/api/auth/login`, {
     // const response = await fetch(`http://localhost:8999/api/auth/login`, {
 
       method: "POST",
